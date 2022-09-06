4a42c8a77a286a0d51990b92ab7f96ac966d29df
Luann Ferreira <luo.luann@gmail.com>
2022-09-06 18:44:25 -0300

    Uppade to model-user class

--- Pentagon/src/main/java/com/jlvlg/pentagon/models/User.java
+++ Pentagon/src/main/java/com/jlvlg/pentagon/models/User.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  * User Object Class: Inherits Followable Objec Abstractt Class
  */
 
-public class User {
+public class User extends Followable {
 	
 	private Long id;
 	private String name;
