6cc91a279d539c0e6a191312912d8668d24d13f2
Lucas Vinícius <lucas@jlvlg.com>
2022-09-11 18:52:53 -0300

    Factory interface

---
+++ src/test/java/com/jlvlg/pentagon/factories/Factory.java
@@ -0,0 +1,10 @@
+package com.jlvlg.pentagon.factories;
+
+/**
+ * A simple interface implemented by every factory
+ * @author lucas
+ *
+ */
+public interface Factory {
+	public Factory getInstance();
+}
