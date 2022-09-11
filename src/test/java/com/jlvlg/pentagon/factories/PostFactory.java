e565f13f19fe1741dc27908f81a76f1b85308ac0
Lucas Vinícius <lucas@jlvlg.com>
2022-09-11 19:49:27 -0300

    Created PageFactory, PostFactory and UserFactory

---
+++ src/test/java/com/jlvlg/pentagon/factories/PostFactory.java
@@ -0,0 +1,27 @@
+package com.jlvlg.pentagon.factories;
+
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+import com.jlvlg.pentagon.models.Post;
+
+/**
+ * Contains a single static generate method that returns a Post object with random attribute values
+ * @author lucas
+ *
+ */
+public class PostFactory {
+	/**
+	 * Generates a post
+	 * @return A Post object with random attribute values
+	 */
+	public static Post generate() {
+		Faker faker = new Faker(new Locale("pt-BR"));
+		return new Post(
+				UserFactory.generate(),
+				PageFactory.generate(),
+				faker.lorem().paragraph(),
+				faker.lorem().sentence()
+			);
+	}
+}
