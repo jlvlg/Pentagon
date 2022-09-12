727a2020119def039ef48c7051d96b560e59c124
Luann Ferreira <luo.luann@gmail.com>
2022-09-12 01:00:32 -0300

    Post UnitTest Class

---
+++ src/test/java/com/jlvlg/pentagon/unittest/PostUnitTest.java
@@ -0,0 +1,48 @@
+package com.jlvlg.pentagon.unittest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.jlvlg.pentagon.factories.PostFactory;
+import com.jlvlg.pentagon.factories.UserFactory;
+import com.jlvlg.pentagon.models.Post;
+import com.jlvlg.pentagon.models.User;
+
+
+/**
+ * Post JUnit Test Class: Methods turnVisibleTo and turnInvisibleTo Tested
+ * @author Luann
+ */
+
+public class PostUnitTest {
+	private Post post;
+	private User visibility;
+	
+	@BeforeEach
+	public void setup() {
+		post = PostFactory.generate();
+		visibility = UserFactory.generate();
+	}
+	
+	@Test
+	@DisplayName("Allowing post visibility for a specific user")
+	void turnVisibleTo() {
+		assertDoesNotThrow(()-> post.turnVisibleTo(visibility));
+		assertEquals(visibility, post.getVisibility().get(0));
+	}
+	@Test
+	@DisplayName("Disallowing post visibility for a specific user")
+	void turnInvisibleTo() {
+		//assertDoesNotThrow(()-> post.turnVisibleTo(visibility));
+		assertDoesNotThrow(()-> post.turnVisibleTo(UserFactory.generate()));
+		assertDoesNotThrow(()-> post.turnVisibleTo(visibility));
+		assertEquals(visibility, post.getVisibility().get(1));
+		assertDoesNotThrow(()-> post.turnInvisibleTo(visibility));
+		assertFalse(()-> post.getVisibility().contains(visibility));
+	}
+}
